9b0a8530ea84fa364fbe80be058c54a05dc34aa5
unknown <elojude44@gmail.com>
2022-06-25 15:37:13 -0700

    first calculator task

---
+++ calc.js
@@ -0,0 +1,9 @@
+
+let pent = '0';
+
+if(pent==='5'){console.log('pentagon is 5 sides');} else if(pent==='0'){console.log('data is not true');} else(console.log('get out'))
+
+let num = 0
+for (let i=0; i<100; i+=5){num += i; console.log(i);}
+
+console.log(num)
